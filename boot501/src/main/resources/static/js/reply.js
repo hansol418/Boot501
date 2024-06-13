df8d6e923597ae294adc3d4977f73400b1c5c834
LeeHanSol <hslee4954@gmail.com>
2024-06-13 17:02:23 +0900

    20240613 오후 수업 커밋

--- boot501/src/main/resources/static/js/reply.js
+++ boot501/src/main/resources/static/js/reply.js
@@ -13,8 +13,44 @@ async function getTest(bno) {
 // goLast: 마지막에 최신 댓글이 있다면, 마지막 댓글로 가는 여부.
 async function getList({bno,page,size,goLast}){
     const result = await axios.get(`/replies/list/${bno}`,{params : {page,size}})
+
+    // 마지막 댓글 보러가기
+    if(goLast) {
+        const total = result.data.total
+        const lastPage = parseInt(Math.ceil(total/size))
+        return getList({bno:bno, page:lastPage, size:size})
+    }
+
     return result.data
 }
+
+//댓글 등록, 모달창으로 작업하기.
+async function addReply(replyObj){
+    const response = await axios.post(`/replies/`,replyObj)
+    return response.data
+}
+
+// 조회
+async function getReply(rno){
+    const response = await axios.get(`/replies/${rno}`)
+    return response.data
+}
+
+// 수정
+async function updateReply(replyObj){
+    console.log("replyObj 확인: " + replyObj.rno)
+    console.log("replyObj 확인2: " + replyObj.replyText)
+    const response = await axios.put(`/replies/${replyObj.rno}`,replyObj)
+    return response.data
+}
+
+// 삭제
+async function deleteReply(rno){
+
+    const response = await axios.delete(`/replies/${rno}`)
+    return response.data
+}
+
 
 
 
